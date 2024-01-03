0522594fa2a54e80a1f651096896618f4987e316
changu100 <chad@101.inc>
2024-01-03 10:18:59 +0900

    feat: set swagger

---
+++ src/main/kotlin/com/example/demo/config/SwaggerConfig.kt
@@ -0,0 +1,22 @@
+package com.example.demo.config
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig {
+    @Bean
+    fun getSwaggerConfig() : OpenAPI{
+        return OpenAPI()
+            .components(Components())
+            .info(
+                Info()
+                    .title("ToDoList API")
+                    .description("schema")
+                    .version("1.0.0")
+            )
+    }
+}
